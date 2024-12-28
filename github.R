6a376d82a1af958d3ed5254056f51d717b31e4c5
burnout909 <newhorizon2232@gmail.com>
2024-12-28 18:30:13 +0900

    github connected code but do not run : already connected

---
+++ github.R
@@ -0,0 +1,53 @@
+###### 별도 설정 없이도 push 잘 되는 관계로 해당 내용은 skip ##########
+
+#### 1. Sign up at GitHub.com ################################################
+
+## If you do not have a GitHub account, sign up here:
+## https://github.com/join
+
+# ----------------------------------------------------------------------------
+
+#### 2. Install git ##########################################################
+
+## If you do not have git installed, please do so: 
+## Windows ->  https://git-scm.com/download/win
+## Mac     ->  https://git-scm.com/download/mac
+## Linux   ->  https://git-scm.com/download/linux
+##             or: $ sudo dnf install git-all
+
+# ----------------------------------------------------------------------------
+
+### 3. Configure git with Rstudio ############################################
+
+## Name, email
+usethis::use_git_config(user.name = "minseong kim", user.email = "znlwmdhkd1@yonsei.ac.kr")
+
+## setting: permanent storage
+credentials::credential_helper_set("manager")                 # Windows
+credentials::credential_helper_set("osxkeychain")             # Mac
+credentials::credential_helper_set("store")                   # Linux
+
+## create a personal access token for authentication:
+usethis::create_github_token() 
+## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
+
+## copy and paste your token and press enter
+credentials::set_github_pat()
+
+# ----------------------------------------------------------------------------
+
+#### 4. Restart R! ###########################################################
+
+# ----------------------------------------------------------------------------
+
+#### 5. Verify settings ######################################################
+
+usethis::git_sitrep()
+
+## Your username and email should be stated correctly in the output. 
+## Also, the report shoud cotain something like:
+## 'Personal access token: '<found in env var>''
+
+# ----------------------------------------------------------------------------
+
+## THAT'S IT!
